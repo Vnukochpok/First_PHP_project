0e5fe8c8accd82170e38b45acf05d62a57a7e2a9
Andrey Zhdanov <CayMan.1@bk.ru>
2025-08-22 14:53:52 +0700

    Добавление блоков страниц

---
+++ project/blocks/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title><?=$title?></title>
+</head>
+<body>
+    <a href="index.php">Главная страница</a>
+    <a href="calculator.php">Калькулятор</a>
+    <a href="about.php">Информация о сайте</a>
+    <a href="registration.php">Зарегистрироваться на сайте</a>
+    

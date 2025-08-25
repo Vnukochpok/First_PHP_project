cb8d2ae994ff518e25f60958dd14fe0ed5ff2f51
Andrey Zhdanov <CayMan.1@bk.ru>
2025-08-25 18:56:52 +0700

    поля для введения информации для входа

---
+++ project/lk.php
@@ -0,0 +1,18 @@
+<?php
+    $title = "Вход в личный кабинет";
+    require("blocks/header.php");
+?>
+
+
+<form action="lk_check.php" method="post">
+    <input type="email" name="email" placeholder="Введите ваш email" class="form-control"><br>
+        <input type="text" name="password" placeholder="Введите пароль" class="form-control"><br>
+        <input type="submit" value="Отправить" class="btn btn-success">
+    </form>
+
+
+
+<?php
+
+require("blocks/footer.php");
+?>

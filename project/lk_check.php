b11bb0f3b9d99fb4a30e842bd8f80ea001ca3cc7
Andrey Zhdanov <CayMan.1@bk.ru>
2025-08-25 18:57:34 +0700

    проверка данных для входа в личный кабинет

---
+++ project/lk_check.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project_db";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+//поиск пользователя в БД
+$login_qu="SELECT * FROM users WHERE email='$email'";
+$login_qu_res=mysqli_query($conn, $login_qu);
+
+
+if (/*подсчет строк в ответе*/mysqli_num_rows($login_qu_res) == 1) {
+    $user = mysqli_fetch_assoc($login_qu_res);
+    if (/*проверка пароля по ХЭШУ*/password_verify($password, $user['password'])) {
+      echo "успешный вход в лк";
+      // session_start();
+      // $_SESSION['email'] = $user['email'];
+      // header("Location: index.php");
+    } else {
+      echo "Неверный пароль.";
+    }
+  } else {
+    echo "Пользователь не найден.";
+  }
+?>
